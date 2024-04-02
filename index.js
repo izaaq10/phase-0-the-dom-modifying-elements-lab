283665400d1f39f7968a3efd5c98ef3dffd99b26
Izaaq <odongoizaaq@gmail.com>
2024-04-02 08:03:42 -0400

    Adds the dom-modifying-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//creating new elements
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+//appending
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader)
